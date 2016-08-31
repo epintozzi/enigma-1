03f64e6547d71d5449ee66f794258a82da93edc9
Erin Pintozzi <erin.pintozzi@gmail.com>
2016-08-30 21:14:55 -0600

    making classes work correctly together

--- lib/encryptor.rb
+++ lib/encryptor.rb
@@ -7,19 +7,19 @@ class Encryptor
   attr_accessor :offset, :key
 
   def initialize
-    @offset = OffsetGenerator.new
     @key = Key.new
   end
 
   def char_map
-    @alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z .)
+    @alphabet = %w(a b c d e f g h i j k l m n o p q r s t u v w x y z)
   end
 
-  def encrypt(message)
+  def encrypt(message, key = nil, date = Date.today)
 
     characters = message.chars
-    @our_key = @key.key_generator
-    @date = @offset.date_generator
+    @our_key = key || @key.key_generator
+    # binding.pry
+    @offset = OffsetGenerator.new(@our_key, date)
     @encrypted_message = []
 
     characters.each_with_index do |char, index|
@@ -32,58 +32,20 @@ class Encryptor
       elsif index % 4 == 3
         offset = @offset.fourth_offset
       end
-      @rotated_character = char_map.rotate(char_map.index(char) + offset).first
-      @encrypted_message << @rotated_character
+
+      if char_map.include?(char)
+        @rotated_character = char_map.rotate(char_map.index(char) + offset).first
+        @encrypted_message << @rotated_character
+      else
+        @encrypted_message << char
       end
 
-    return offset = @encrypted_message.join
-
+    end
+    return @encrypted_message.join
   end
-
 end
 
-# puts e = Encryptor.new
-# # # # my_message = "this is so secret ..end.."
-# puts output = e.encrypt("hellothisisalongword..")
-#
-
-#   def encrypt(message)
-#     @message = message
-#     @characters = message.chars
-#     encrypted_letters = []
-#     @characters.each do |letter|
-#       if @rotation_place % 4 == 0
-#         offset = self.first_key_rotation
-#         @encrypted_letters << @alphabet.rotate(offset)
-#         @rotation_place += 1
-#       elsif @rotation_place % 4 == 1
-#         offset = self.second_key_rotation
-#         @alphabet.rotate(offset)
-#         @rotation_place += 1
-#       elsif @rotation_place % 4 == 2
-#         offset = self.third_key_rotation
-#         @alphabet.rotate(offset)
-#         @rotation_place += 1
-#       elsif @rotation_place % 4 == 3
-#         offset = self.fourth_key_rotation
-#         @alphabet.rotate(offset)
-#         @rotation_place += 1
-#       end
-#       # @encrypt_letter = alphabet.rotate(offset)
-#       encrypted_letters.join
-#     end
-#   end
-#
-#
-#
-# puts key.encrypt_letter("l")
-# puts key.encrypt("hi")
-# letters = phrase.chars
-#       encrypted_letters = letters.map do |letter|
-#         decrypt_letter(letter)
-#       end
-#       encrypted_letters.join
-#   end
-
-
-#
+  e = Encryptor.new
+  message = "hello..end.."
+  # puts e.@key
+  puts e.encrypt(message)
